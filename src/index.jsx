5015e5381feb402e0535017bdc33be45b2beb4c2
Ibukunoluwa Salau <salau_ibukunoluwa@yahoo.com>
2025-01-26 20:50:51 +0000

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,4 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(<App />)
+console.log("Initializing app");
+const root = createRoot(document.getElementById('root'));
+console.log("Root element found:", document.getElementById('root'));
+root.render(<App />);
+console.log("App rendered");
