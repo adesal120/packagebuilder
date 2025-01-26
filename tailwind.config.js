02227ac46cbaad585cdb452f7a13609e520fae66
Ibukunoluwa Salau <salau_ibukunoluwa@yahoo.com>
2025-01-26 21:17:53 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust this based on your project
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
