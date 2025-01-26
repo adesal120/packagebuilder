a0139dc63e03efbaa60ac8a2e200ba6be33e5b43
Ibukunoluwa Salau <salau_ibukunoluwa@yahoo.com>
2025-01-26 21:28:34 +0000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss'),
-    require('autoprefixer'),  // This might also be needed depending on your setup
-  ],
-};
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
